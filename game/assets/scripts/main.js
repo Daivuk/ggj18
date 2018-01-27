82c866745c02a8c7a51648e3f3675efed7c29868
daivuk <daivuk@gmail.com>
2018-01-26 20:34:43 -0500

    Centered map

--- game/assets/scripts/main.js
+++ game/assets/scripts/main.js
@@ -1,11 +1,38 @@
+var tiledMap = getTiledMap("arena.tmx");
+var cameraTransform = new Matrix();
+var invCameraTransform = new Matrix();
+var resolution = new Vector2(560, 304);
+
+function updateCamera()
+{
+    // Fit the map into the display
+    var screenRes = Renderer.getResolution();
+    var scale = screenRes.x / resolution.x;
+    var newH = resolution.y * scale;
+    var diffH = screenRes.y - newH;
+    cameraTransform = 
+        Matrix.createTranslation(new Vector3(0, diffH / scale / 2, 0)).mul(
+        Matrix.createScale(new Vector3(scale, scale, 1)));
+}
+
 function update(dt)
 {
+    // Quit game with escape
+    if (Input.isJustDown(Key.ESCAPE)) quit();
 
+    updateCamera();
 }
 
 function render()
 {
     Renderer.clear(new Color(0, 0, 0, 1));
+
+    SpriteBatch.begin(cameraTransform);
+
+    // Render the map
+    tiledMap.render();
+
+    SpriteBatch.end();
 }
 
 function renderUI()
