5859c7f7690c77abca669ede7ab4d88e6f8ede49
Frongo <frongosk8r@gmail.com>
2018-01-27 14:39:16 -0500

    Added taser functionality

--- game/assets/scripts/hero.js
+++ game/assets/scripts/hero.js
@@ -2,6 +2,9 @@ var HERO_COLLISION_SIZE = 10;
 var HERO_SPEED = 50;
 var HERO_PICKUP_OFFSET = new Vector2(0, 0);
 var HERO_GLYPH_COLOR = new Color(1.0, 0.8, 0.0);
+var HERO_DISABLE_TIME = 2;
+var HERO_TASER_USE_INTERVAL = 3;
+var HERO_TASER_CHARGE_TIME = 1;
 
 function hero_create(_index, _pos, _color)
 {
@@ -9,7 +12,14 @@ function hero_create(_index, _pos, _color)
         index: _index,
         position: new Vector2(_pos),
         color: new Color(_color),
-        dir: "w"
+        dir: "w",
+        taserEnabled: false,
+        taserTimer: HERO_TASER_USE_INTERVAL,
+        taserOn: false,
+        taserCharge: 0,
+        tasered: false,
+        disabled: false,
+        disableTimer: HERO_DISABLE_TIME
     };
 
     hero.spriteAnim = playSpriteAnim("hacker" + _index + ".spriteanim", "idle_e");
@@ -110,6 +120,11 @@ function hero_renderGlow(hero)
 
 function hero_update(hero, dt)
 {
+    if(hero.disabled)
+    {
+        return;
+    }
+
     var leftThumb = GamePad.getLeftThumb(hero.index);
 
     // Get the next position according to thumb movement
@@ -137,12 +152,104 @@ function hero_update(hero, dt)
 
     hero.spriteAnim.play(anim + "_" + hero.dir)
 
+    // Pick up items
     var pickup = pickups_acquire(hero.position.add(HERO_PICKUP_OFFSET));
 
     if(pickup != null)
     {
         print("picked up " + pickup.glyph);
         hero_revealGlyph(hero, pickup.glyph);
+    }
+
+    // Handle taser
+    if(hero.taserEnabled)
+    {
+        if(GamePad.isDown(hero.index, Button.A))
+        {
+            if(!hero.taserOn)
+            {
+                print("hero " + hero.index + " taser on");
+                hero.taserOn = true;
+            }
+
+            hero.taserCharge += dt;
+
+            if(hero.taserCharge > HERO_TASER_CHARGE_TIME)
+            {
+                hero.taserCharge = HERO_TASER_CHARGE_TIME + 1;
+            }
+        }
+        else
+        {
+            if(hero.taserOn)
+            {
+                var chargeAmount = hero.taserCharge;
+                hero.taserOn = false;
+                hero.taserEnabled = false;
+                hero.taserCharge = 0;
+
+                if(chargeAmount < HERO_TASER_CHARGE_TIME)
+                {
+                    print("hero " + hero.index + " fired taser without enough charge");
+                    return;
+                }
+
+                print("hero " + hero.index + " fired taser");
+
+                for(var i = 0; i < heroes.length; ++i)
+                {
+                    if(i == hero.index)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        var otherHero = heroes[i];
+
+                        if(Vector2.distance(hero.position, otherHero.position) < 10)
+                        {
+                            hero_tasered(otherHero);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    else
+    {
+        hero.taserTimer -= dt;
+
+        if(hero.taserTimer < 0)
+        {
+            hero.taserEnabled = true;
+            hero.taserTimer = HERO_TASER_USE_INTERVAL;
+        }
+    }
+}
+
+function hero_taser_update(hero, dt)
+{
+    if(hero.tasered || hero.disabled)
+    {
+        hero.disabled = true;
+
+        hero.disableTimer -= dt;
+
+        if(hero.disableTimer < 0)
+        {
+            hero.disabled = false;
+            hero.tasered = false;
+            hero.disableTimer = HERO_DISABLE_TIME;
+        }
+    }
+}
+
+function hero_tasered(hero)
+{
+    if(!hero.disabled && !hero.tasered)
+    {
+        print("hero " + hero.index + " tasered");
+        hero.tasered = true;
     }
 }
 
@@ -151,5 +258,10 @@ function heroes_update(dt)
     for(var i = 0; i < heroes.length; ++i)
     {
         hero_update(heroes[i], dt);
+    }
+
+    for(var i = 0; i < heroes.length; ++i)
+    {
+        hero_taser_update(heroes[i], dt);
     }
 }
