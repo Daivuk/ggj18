3ff888f0733c2bea8fefef3c4ad41ba4df691342
Frongo <frongosk8r@gmail.com>
2018-01-26 21:55:53 -0500

    Set up sprite on character

--- game/assets/scripts/hero.js
+++ game/assets/scripts/hero.js
@@ -1,5 +1,5 @@
 var HERO_COLLISION_SIZE = 10;
-var HERO_SPEED = 100;
+var HERO_SPEED = 50;
 
 var hacker = playSpriteAnim("hacker.spriteanim", "idle");
 
@@ -11,17 +11,36 @@ function hero_create(_index, _pos, _color)
         color: new Color(_color)
     };
 
+    hero.spriteAnim = playSpriteAnim("hacker.spriteanim", "idle_e");
+
     return hero;
 }
 
 function hero_render(hero)
 {
-    SpriteBatch.drawSpriteAnim(hacker, hero.position);
+    SpriteBatch.drawSpriteAnim(hero.spriteAnim, hero.position);
 }
 
 function hero_update(hero, dt)
 {
-    var nextPosition = new Vector2(hero.position.add(GamePad.getLeftThumb(hero.index).mul(dt * HERO_SPEED)));
+    var leftThumb = GamePad.getLeftThumb(hero.index);
 
+    // Get the next position according to thumb movement
+    var nextPosition = new Vector2(hero.position.add(leftThumb.mul(dt * HERO_SPEED)));
+
+    // Apply collision to the movement
     hero.position = tiledMap.collision(hero.position, nextPosition, new Vector2(HERO_COLLISION_SIZE, HERO_COLLISION_SIZE));
+
+    // Point the character in the right direction
+    if(Math.abs(leftThumb.x) > 0.001)
+    {
+        if(leftThumb.x > 0)
+        {
+            hero.spriteAnim.play("idle_e")
+        }
+        else
+        {
+            hero.spriteAnim.play("idle_w")
+        }
+    }
 }
